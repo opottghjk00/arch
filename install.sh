ed31bf17731bac44471b9e7c8877b970b89e3d21
opottghjk00 <opottghjk00@tutanota.com>
2021-10-12 13:49:04 +0800

    commit

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # use the command "sudo ./install.sh"
+# current working directory is $HOME/repo/github/os_set/arch
+
 
 
 # package manager -aura
+
 #tar xvzf ./source/aura-bin.tar.gz
 #cd ./source/aura-bin
 #makepkg
@@ -10,46 +13,35 @@
 
 
 # basic utility
-#sudo aura -S git unzip stow gcin noto-fonts-emoji noto-fonts-cjk pandoc texlive-most texlive-lang pass gvim alsa-utils xclip npm wget python-pip man-db exa ninja tk tcl xmonad-contrib pulseaudio pulseaudio-bluetooth rofi libnotify lynx cronie zathura-pdf-mupdf
+#sudo aura -S git unzip stow gcin noto-fonts-emoji noto-fonts-cjk pandoc texlive-most texlive-lang pass gvim alsa-utils xclip npm wget python-pip man-db exa ninja tk tcl xmonad-contrib pulseaudio pulseaudio-bluetooth rofi libnotify lynx cronie zathura-pdf-mupdf cairo-dock-plug-ins
 #pip install ueberzug
-# basic application
-#sudo aura -S alacritty qutebrowser nvim nitrogen picom ranger zathura calcurse mpv r xmonad mpd ncmpcpp pulsemixer dunst
-#sudo aura -A brave-bin polybar notion-app mutt-wizard abook miniconda3
-#sudo mv /usr/bin/nvim /usr/bin/v
 
+# basic application
+#sudo aura -S alacritty qutebrowser nvim nitrogen picom ranger zathura calcurse mpv r xmonad mpd ncmpcpp pulsemixer dunst lxappearance qt5ct pcmanfm cairo-dock
+#sudo aura -A brave-bin polybar notion-app mutt-wizard abook miniconda3 qt5-webengine-widevine
 
 
 # wallpaper
-# ! [ -d $HOME/Document/Picture/wallPaper ] && mkdir -p $HOME/Document/Picture/wallPaper
-#git clone https://gitlab.com/dwt1/wallpapers.git $HOME/Document/Picture/wallPaper/dt
-#git clone https://github.com/BrodieRobertson/wallpapers.git $HOME/Document/Picture/wallPaper/brodie
+# ! [ -d $HOME/Document/picture/ ] && mkdir -p $HOME/Document/picture/
+#cp -r ./source/wallpaper ~/Document/picture/wallpaper
 
 
 
 # wifi driver
 #sudo pacman -S base-devel dkms bc   # dependencies
-#cd $HOME/repo/github/linux_setup/arch/driver/rtl8821ce_wifi_driver
+#cd $HOME/repo/github/os_setup/arch/driver/rtl8821ce_wifi_driver
 #sudo ./dkms-install.sh
 
 
 
 # bluetooth driver
-#cd $HOME/repo/github/linux_setup/arch/driver/asusBt500_bluetooth_driver/usb
+#cd $HOME/repo/github/os_setup/arch/driver/asusBt500_bluetooth_driver/usb
 #sudo make install
 
 #sudo pacman -S bluez bluez-utils                 # dependencies
 #sudo modprobe btusb                              # load the module 
 #sudo systemctl enable bluetooth.service          # enable/start the service
 #sudo systemctl start bluetooth.service
-
-
-# qutebrowser netflix fix
-#tar xvzf ./source/qt5-webengine-widevine.tar.gz
-#cd ./source/qt5-webengine-widevine
-#makepkg
-#sudo aura -U qt5-webengine-widevine-93.0.4577.63-1-x86_64.pkg.tar.zst
-
-
 
 
 # slock
@@ -63,6 +55,11 @@
 
 # zsh
 #sudo aura -S zsh zsh-completions
+
+# themes
+#cd source/themes
+#sudo cp -r Nordic-v40/ Nordic-darker-v40/ Nordic-bluish-accent-v40 /usr/share/themes
+#sudo cp -r Nordzy-cursors/ Nordzy-cursors-white/ Zafiro-Icons-Dark/ Zafiro-Icons-Light/ candy-icons/ /usr/share/icons
 
 
 # configure the application
