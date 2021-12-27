cf4ab62bfc067dc6fe268a47db9f679622d41fad
opottghjk00 <opottghjk00@gmail.com>
2021-12-27 18:49:06 +0800

    add

--- install.sh
+++ install.sh
@@ -147,15 +147,6 @@ function virt_manager_set() {
 }
 
 
-#function nvim-dataScience-setup() {
-#    nvim -jupyter notebook set
-#    pip install jupyter_ascending
-#    jupyter nbextension install --py --sys-prefix jupyter_ascending
-#    jupyter nbextension     enable jupyter_ascending --sys-prefix --py
-#    jupyter serverextension enable jupyter_ascending --sys-prefix --py
-#}
-
-
 function configuration() {
     echo "install the package manager aura"
     aura_install
