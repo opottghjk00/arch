1d9921637467d10d229c4740063147618872907a
opottghjk00 <opottghjk00@gmail.com>
2022-06-12 23:31:55 +0800

    update the installation script and packages

--- README.md
+++ README.md
@@ -1,28 +1,7 @@
 # working directory
-set the working at $HOME/repo/arch/
+set the working at ~/repo/arch
 
 # install
 ```{sh}
 ./install.sh
 ```
-
-
-# some repo that I fork
-
-[suckless st terminal](https://st.suckless.org)
-
-[suckless slock screen locker](https://tools.suckless.org/slock/)
-
-[cypher punk grub theme](https://github.com/anoopmsivadas/Cyberpunk-GRUB-Theme)
-
-
-# general theme and icon theme
-
-[candy icons](https://github.com/EliverLara/candy-icons)
-
-[Nordic theme](https://github.com/EliverLara/Nordic)
-
-[Nordzy cursor theme](https://github.com/alvatip/Nordzy-cursors)
-
-[Zafiro icon theme](https://github.com/zayronxio/Zafiro-icons)
-
--- install.sh
+++ install.sh
@@ -1,93 +1,118 @@
 #!/bin/sh
 
-##################################################################
-#		             function 			                         #
-##################################################################
 
-function aura_install() {
-    git clone https://aur.archlinux.org/aura-bin.git $HOME/repo/arch/res/repo/aura
-    cd $HOME/repo/arch/res/repo/aura
-    makepkg
-    sudo pacman -U *.pkg.tar.zst
+##################################################
+#		             function 			         #
+##################################################
+
+# basic packages install
+function basic_packages_install(){
+    sudo pacman -S linux-lts-headers base-devel gcin noto-fonts-emoji noto-fonts-cjk dkms bc
+    sudo pacman -S xorg-xinit xorg-xsetroot xorg-server imlib2 xorg-xrandr
+    sudo pacman -S zsh zsh-completions pass git unzip
+    sudo pacman -S xclip npm wget man-db exa ninja tk tcl xmonad-contrib libnotify fzf
+    sudo pacman -S alacritty nitrogen mpv r xmonad dunst lxappearance pcmanfm rofi starship
 }
 
 
-function packages_install() {
-    sudo aura -S unzip stow gcin noto-fonts-emoji noto-fonts-cjk pandoc texlive-most texlive-lang pass gvim alsa-utils 
-    sudo aura -S xclip npm wget man-db exa ninja tk tcl xmonad-contrib pulseaudio pulseaudio-bluetooth libnotify 
-    sudo aura -S zathura-pdf-mupdf fzf zsh zsh-completions xorg-xinit xorg-xsetroot xorg-server imlib2 xorg-xrandr
-    sudo aura -S alacritty qutebrowser nitrogen ranger r mpd ncmpcpp pulsemixer dunst lxappearance pcmanfm rofi starship
-    # sudo aura -S mpv (errror)
-    sudo aura -A miniconda3 brave-bin polybar qt5-webengine-widevine grive nvim-packer-git picom-jonaburg-git
-    # sudo aura -A mutt-wizard abook
-    pip install ueberzug PyPtt
-    sudo conda install pandas matplotlib numpy scipy seaborn scikit-learn statsmodels
+# install AUR helper
+function AUR_packages_install(){
+    [[ ! -d $HOME/Document ]] && mkdir $HOME/Document
+    mkdir -p $HOME/Document/open_source
+    git clone https://aur.archlinux.org/yay.git $HOME/open_source/yay
+    cd $HOME/open_source/yay
+    makepkg -si
+    sudo yay -S brave-bin polybar miniconda3 grive picom-jonaburg-git notion-app
 }
 
 
-function dotfile_set() {
-    chsh -s /bin/zsh
-    cd $HOME
-    [[ ! -d .config ]] && mkdir .config
-    [[ ! -d .local ]] && mkdir .local
-    mkdir -p .local/bin .local/share
-    git clone https://github.com/opottghjk00/dotx.git
+# configure the dotfile
+function dotfile_set(){
+    sudo pacman -S stow
+    [[ ! -d $HOME/.config ]] && mkdir $HOME/.config
+    [[ ! -d $HOME/.local ]] && mkdir $HOME/.local
+    mkdir -p $HOME/.local/bin $HOME/.local/share
+    git clone https://github.com/opottghjk00/dotx.git $HOME/dotx
     cd $HOME/dotx
     stow */ 
-    # sudo cp -f $HOME/.config/mutt/mutt-wizard.muttrc /usr/share/mutt-wizard/mutt-wizard.muttrc
+}
+
+function additional_system_setup(){
+    sudo cp $HOME/repo/archSetup/res/utils/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+}
+
+# setup documentation work
+function documentation_setup(){
+    sudo pacman -S gvim neovim zathura evince pandoc texlive-most texlive-lang zathura-pdf-mupdf
+    git clone https://github.com/opottghjk00/nvimIDE.git $HOME/repo/nvimIDE
+    ln -s $HOME/repo/nvimIDE $HOME/.config/nvim
 }
 
 
-function wallPaper_set() {
-    mkdir -p $HOME/Document/picture/
-    cp -r $HOME/repo/arch/res/wallPaper/ ~/Document/picture/
+# setup sound system
+function sound_system_setup(){
+    sudo pacman -S pipewire pipewire-pulse
+    systemctl start pipewire-pulse.service
 }
 
 
-function basic_config() {
-    # enable tap click
-    cd $HOME/repo/arch/res/setup_file
-    sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/
-    # screen locker
-    cd $HOME/repo/arch/res/repo/slock_rice
-    sudo make clean install
-    # another terminal
-    cd $HOME/repo/arch/res/repo/st_rice
-    sudo make clean install
-    # grub theme
-    cd $HOME/repo/arch/res/repo/grub_theme
-    sudo ./install.sh
+# wallpaper
+function wallPaper_setup(){
+    mkdir -p $HOME/Document/picture/wallpaper
+    ln -s $HOME/repo/archSetup/res/wallpaper ~/Document/picture/wallpaper
 }
 
 
-function driver_setup(){
-    # bluetooth driver
-    cd $HOME/repo/arch/res/driver/asusBt500_bluetooth_driver/usb
-    sudo make install
-    sudo aura -S bluez bluez-utils                   # dependencies
-    sudo modprobe btusb                              # load the module 
-    sudo systemctl enable bluetooth.service          # enable/start the service
-    sudo systemctl start bluetooth.service
-    # wifi deiver
-    git clone https://github.com/tomaspinho/rtl8821ce.git $HOME/repo/arch/res/driver/wifi_driver
-    cd $HOME/repo/arch/res/driver/wifi_driver
+# set wifi driver
+function wifiDrier_setup(){
+    # if no wifi
+    # cd $HOME/repo/arch/driver/rtl8821ce_wifi_driver
+    # sudo ./dkms-install.sh
+    git clone https://github.com/tomaspinho/rtl8821ce.git $HOME/Document/open_source/rtl8821ce
+    cd $HOME/Document/open_source/rtl8821ce
     sudo ./dkms-install.sh
 }
 
 
-function repo_clone() {
-    cd $HOME/repo/
-    git clone https://github.com/opottghjk00/leet_code_practice.git
-    git clone https://github.com/opottghjk00/sudo_random_password_generator.git
-    git clone https://github.com/opottghjk00/ptt_crawler.git
-    git clone https://github.com/opottghjk00/newsDL.git
+# set bluetooth driver
+function bluetoothDiver_setup(){
+    cd $HOME/repo/arch/driver/asusBt500_bluetooth_driver/usb
+    sudo make install
+    sudo pacman -S bluez bluez-utils                 # dependencies
+    sudo modprobe btusb                              # load the module 
+    sudo systemctl enable bluetooth.service          # enable/start the service
+    sudo systemctl start bluetooth.service
 }
 
 
-function themes_set() {
-    cd $HOME/repo/arch/source/themes
+# clone repo
+function repo_clone(){
+    git clone https://github.com/opottghjk00/slock_rice.git $HOME/repo/slock_rice
+    git clone https://github.com/opottghjk00/st_rice.git $HOME/repo/st_rice
+    git clone https://github.com/opottghjk00/leet_code_practice.git $HOME/repo/leet_code_practice
+    git clone https://github.com/opottghjk00/dwm_rice.git $HOME/repo/dwm_rice
+    git clone https://github.com/opottghjk00/BasicIntegration.jl $HOME/repo/BasicIntegration.jl
+    cd $HOME/repo/slock_rice
+    sudo make clean install
+    cd $HOME/repo/st_rice
+    sudo make clean install
+}
+
+
+# setup themes
+function themes_setup(){
+    cd $HOME/repo/archSetup/res/themes
     sudo cp -r Nordic-v40/ Nordic-darker-v40/ Nordic-bluish-accent-v40/ /usr/share/themes
     sudo cp -r Nordzy-cursors/ Nordzy-cursors-white/ Zafiro-Icons-Dark/ Zafiro-Icons-Light/ candy-icons/ /usr/share/icons
+    cd $HOME/repo/archSetup/res/themes/grub_theme
+    sudo ./install.sh
+}
+
+
+function google_drive_setup(){
+    mkdir -p $HOME/Document/google-drive
+    cd $HOME/Document/google-drive
+    grive -f
 }
 
 
@@ -99,74 +124,77 @@ function virt_manager_set() {
 }
 
 
-#function google_drive_set() {
-#    mkdir -p $HOME/Document/google-drive
-#    cd $HOME/Document/google-drive
-#    grive -a
-#}
+function configuration() {
+    echo "###########################################"
+    echo "###       basic packages install       ###"
+    echo "###########################################"
+    basic_packages_install()
+    echo -e "\n\n"
+
+    echo "###########################################"
+    echo "###         AUR packages install        ###"
+    echo "###########################################"
+    AUR_packages_install()
+    echo -e "\n\n"
+
+    echo "###########################################"
+    echo "###            dot file set             ###"
+    echo "###########################################"
+    dotfile_set()
+    echo -e "\n\n"
+
+    echo "###########################################"
+    echo "###       additional_system_setup       ###"
+    echo "###########################################"
+    additional_system_setup()
+    echo -e "\n\n"
 
 
+    echo "###########################################"
+    echo "###         documentation setup         ###"
+    echo "###########################################"
+    documentation_setup()
+    echo -e "\n\n"
 
-###################################################################
-#		                install process  	                      #
-###################################################################
+    echo "###########################################"
+    echo "###          sound system setup         ###"
+    echo "###########################################"
+    sound_system_setup()
+    echo -e "\n\n"
 
-echo "######################################"
-echo "#  install the package manager aura  #"
-echo "######################################"
-aura_install
-echo -e "\n\n"
+    echo "###########################################"
+    echo "###           wall paper setup          ###"
+    echo "###########################################"
+    wallPaper_setup()
+    echo -e "\n\n"
 
-echo "#####################################"
-echo "#         install packages          #"
-echo "#####################################"
-packages_install
-echo -e "\n\n"
+    echo "###########################################"
+    echo "###          wifi driver setup          ###"
+    echo "###########################################"
+    wifiDrier_setup()
+    echo -e "\n\n"
 
-echo "#####################################"
-echo "#        set up the dot file        #"
-echo "#####################################"
-dotfile_set
-echo -e "\n\n"
+    echo "###########################################"
+    echo "###        bluetooth driver setup       ###"
+    echo "###########################################"
+    bluetoothDiver_setup()
+    echo -e "\n\n"
 
-echo "#####################################"
-echo "#         install wallpaper         #"
-echo "#####################################"
-wallPaper_set
-echo -e "\n\n"
+    echo "###########################################"
+    echo "###             repo clone              ###"
+    echo "###########################################"
+    repo_clone()
+    echo -e "\n\n"
 
-echo "#####################################"
-echo "#         set up the theme          #"
-echo "#####################################"
-themes_set
-echo -e "\n\n"
+    echo "###########################################"
+    echo "###           themes setup              ###"
+    echo "###########################################"
+    themes_setup()
+    echo -e "\n\n"
 
-
-echo "#####################################"
-echo "#        basic configurattion       #"
-echo "#####################################"
-basic_config
-echo -e "\n\n"
-
-echo "#####################################"
-echo "#  set up bluetooth & wifi driver   #"
-echo "#####################################"
-driver_setup
-echo -e "\n\n"
-
-echo "#####################################"
-echo "#      download the remote repo     #"
-echo "#####################################"
-repo_clone
-echo -e "\n\n"
-
-echo "#####################################"
-echo "#        set up virt manager        #"
-echo "#####################################"
-virt_manager_set
-echo -e "\n\n"
-
-echo "#####################################"
-echo "#             reboot...             #"
-echo "#####################################"
-reboot
+    echo "###########################################"
+    echo "###         google_drive_setup          ###"
+    echo "###########################################"
+    google_drive_setup()
+    echo -e "\n\n"
+}
